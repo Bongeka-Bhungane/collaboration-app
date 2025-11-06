a3e0dff07247d24aa073fa40965daa5d9e45cdd2
SineMag. <snenhlanhlamagubane99@gmail.com>
2025-11-06 16:13:43 +0200

    first commit

---
+++ src/server.ts
@@ -0,0 +1,36 @@
+// import express, { Request, Response, Application } from 'express'
+// require('dotenv').config()
+
+// const express = require('express')
+// const app = express()
+// const port = process.env.PORT || 4400
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+// startServer()
+
+import express, {Express, Request, Response, NextFunction} from "express";
+import dotenv from "dotenv";
+import { log } from "console";
+import e = require("express");
+import path = require("path");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.APP_PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+})
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });

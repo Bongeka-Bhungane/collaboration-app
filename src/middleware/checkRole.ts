799ae4411b1702634ab27c0e582c9062bed3639c
Bongeka-Bhungane <angelabhungane@gmsil.com>
2025-11-07 15:47:41 +0200

    created middle ware for auth and roles

---
+++ src/middleware/checkRole.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from "express";
+import { AuthRequest } from "./checkAuth";
+
+export const authorizeRoles = (...allowedRoles: string[]) => {
+    return (req: AuthRequest, res: Response, next: NextFunction) => {
+        if(!req.user || !allowedRoles.includes(req.user.role)) {
+            return res.status(403).json({ message: "Access denied "})
+        } 
+        next();
+    };
+};

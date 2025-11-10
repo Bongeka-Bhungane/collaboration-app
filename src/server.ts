6b7fd9fbc4f0043843ad0622f39139618d3d0cbf
Bongeka-Bhungane <angelabhungane@gmsil.com>
2025-11-10 10:03:54 +0200

    created userRoute and authRoutes, and set them up on server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import { log } from "console";
 import e = require("express");
 import path = require("path");
+import authRoutes from "./routes/authRoutes"
+import userRoutes from "./routes/userRoutes"
 
 dotenv.config();
 
@@ -10,6 +12,9 @@ const app = express();
 const PORT = process.env.APP_PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
